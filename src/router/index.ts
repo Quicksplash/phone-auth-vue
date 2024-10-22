572c6759abb8c4bc0a96e492d2adb80d8a6a6353
Quicksplash <mourzin@gmail.com>
2024-10-22 19:35:49 +0300

    Organize project structure: Atomic Design setup, i18n configuration, Tailwind integration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import AuthPage from '../views/AuthPage.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
-			name: 'home',
-			component: HomeView,
+			name: 'auth',
+			component: AuthPage,
 		},
 	],
 });
---
+++ src/views/AuthPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import AuthTemplate from '@/components/templates/AuthTemplate.vue';
+</script>
+
+<template>
+	<AuthTemplate></AuthTemplate>
+</template>
